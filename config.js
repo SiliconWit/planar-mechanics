5cd9da2d663b149ddda73e5ebe1f85073313f0c7
sammachariaphd <sammacharia.k@gmail.com>
2025-05-15 16:42:52 +0300

    Initial commit for planar-mechanics public repo

---
+++ config.js
@@ -0,0 +1,10 @@
+// src/content/docs/education/planar-mechanics/config.js
+export const planarMechanicsConfig = {
+    label: 'Planar Mechanics',
+    collapsed: true,
+    items: [
+      { label: 'Overview', link: '/education/planar-mechanics/' },
+      { label: '1.1 Position Analysis', link: '/education/planar-mechanics/position-analysis'},
+      // Future topics can be added here by contributors
+    ]
+  };
