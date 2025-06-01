d8c9d62d89485a25f01c2579d2a7b65c53c0a506
sammachariaphd <sammacharia.k@gmail.com>
2025-06-01 22:36:36 +0300

    Automate MDX with common elements; add bionic text to aid quick reading.

--- configs/content.js
+++ configs/content.js
@@ -5,6 +5,7 @@ export const planarMechanicsConfig = {
     items: [
       { label: 'Overview', link: '/education/planar-mechanics/' },
       { label: '1.1 Position Analysis', link: '/education/planar-mechanics/position-analysis'},
+      { label: '1.2 Velocity Analysis', link: '/education/planar-mechanics/velocity-analysis'},
       // Future topics can be added here by contributors
     ]
   };
